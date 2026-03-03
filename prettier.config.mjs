4406d844226d0355b1cc620ba18fe3e15d0bc5bb
Ajmal Nasumudeen <ajmaln73@gmail.com>
2026-03-03 15:06:38 +0530

    Add Prettier configuration and TypeScript declaration for MDViewer component

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 150,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  endOfLine: 'lf',
+}
+
+export default config
+
---
+++ src/types/mdviewer.d.ts
@@ -0,0 +1,7 @@
+declare module "./components/MDViewer.jsx" {
+  import type { ComponentType } from "react";
+
+  const MDViewer: ComponentType;
+  export default MDViewer;
+}
+
